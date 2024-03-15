59e0cf80a8979361041d90170c5758e85ce91d7a
Zied1981 <zkhemiri@gmail.com>
2024-03-15 19:17:14 +0100

    2 push

---
+++ src/components/Stars/Stars.jsx
@@ -0,0 +1,23 @@
+import "./Stars.css";
+const Stars = (props) => {
+  let rating = [];
+  for (let i = 0; i <= 9; i++) {
+    if (props.rate > i && props.rate - i >= 1) {
+      rating.push("full.svg");
+    } else if (props.rate - i >= 0.5) {
+      rating.push("half.svg");
+    } else {
+      rating.push("empty.svg");
+    }
+  }
+
+  return (
+    <div className="star-container">
+      {rating.map((stars, index) => (
+        <img src={`./public/${stars}`} key={index} alt=" stars" />
+      ))}
+    </div>
+  );
+};
+
+export default Stars;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,92 @@
 import "./Home.css";
-import movies from "../../components/movieDatabase/movieDatabase.jsx";
-import { useState } from "react";
+/* import movies from "../../components/movieDatabase/movieDatabase.jsx"; */
+import movies from "../../components/movieDatabase/movieDatabase";
+import { useEffect, useState } from "react";
+import Stars from "../../components/Stars/Stars";
 const Home = () => {
-  const [singleMovie, setSingleMovie] = useState();
+  const [data, setData] = useState(movies);
+  const [sort, setSort] = useState("");
+  useEffect(() => {
+    data.sort((a, b) => {
+      if (sort === "btn1") {
+        return a.year - b.year;
+      } else if (sort === "btn2") {
+        return b.year - a.year;
+      } else if (sort === "btn3") {
+        return b.rate - a.rate;
+      } else if (sort === "btn4") {
+        return ("" + a.title).localeCompare(b.title);
+      } else {
+        return ("" + b.title).localeCompare(a.title);
+      }
+    });
+    setData([...data]);
+  }, [sort]);
+
+  /* const [ascending, setAscending] = useState(false);
+  const [descending, setDescending] = useState(false);
+  const [bestrate, setBestRate] = useState(false);
+  const [az, setAz] = useState(false);
+  const [za, setZa] = useState(false); */
+  /* 
+  useEffect(() => {
+    data.sort((a, b) => {
+      return a.year - b.year;
+    });
+
+    setData([...data]);
+  }, [ascending]);
+
+  useEffect(() => {
+    data.sort((a, b) => {
+      return b.year - a.year;
+    });
+
+    setData([...data]);
+  }, [descending]);
+
+  useEffect(() => {
+    data.sort((a, b) => {
+      return b.rate - a.rate;
+    });
+    setData([...data]);
+  }, [bestrate]);
+  useEffect(() => {
+    data.sort((a, b) => {
+      return ("" + a.title).localeCompare(b.title);
+    });
+
+    setData([...data]);
+  }, [az]);
+
+  useEffect(() => {
+    data.sort((a, b) => {
+      return ("" + b.title).localeCompare(a.title);
+    });
+
+    setData([...data]);
+  }, [za]); */
 
   return (
     <main>
+      <h1>MOVIE FOR YOU</h1>
       <div className="btn-container">
-        <button>Sort by Date Ascending</button>
-        <button>Sort by Date Descending</button>
-        <button>Best Rate</button>
-        <button>A-Z</button>
-        <button>Z-A</button>
+        <button onClick={() => setSort("btn1")}>Sort by Date Ascending</button>
+
+        <button onClick={() => setSort("btn2")}>Sort by Date Descending</button>
+        <button onClick={() => setSort("btn3")}>Best Rate</button>
+        <button onClick={() => setSort("btn4")}>A-Z</button>
+        <button onClick={() => setSort("btn5")}>Z-A</button>
       </div>
       <section className="grid-box">
-        {movies.map((items, index) => (
+        {data.map((items, index) => (
           <article key={index} className="container">
             <h2>{items.title}</h2>
             <p>{items.year}</p>
             <p>{items.director}</p>
             <p>{items.duration}</p>
             <p>{items.rate}</p>
+            <Stars rate={items.rate} />
             <div>
               {items.genre.map((element, index) => (
                 <div key={index}>
